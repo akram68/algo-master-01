b8999a85602bc3137ee6b6d39ce5335a3a53ddc0
akram68 <akram.graichi@se.univ-bejaia.dz>
2025-12-26 15:21:49 +0100

    mod-exo

--- front/src/pages/ExerciseDetail.tsx
+++ front/src/pages/ExerciseDetail.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import { ArrowLeft, CheckCircle, Code, FileText, ListChecks } from 'lucide-react';
-import Quiz from '../components/Quiz';
+import Quiz from '../components/Exercise/quiz';
 import CodeEditor from '../components/Exercise/MonacoEditor/MonacoEditor';
 import { exercisesApi } from '../services/api';
 import { useAuth } from '../context/AuthContext';
